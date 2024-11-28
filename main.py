3f88f63a5dece156e6752a5164b80925d2d44595
Uchechukwu Ekemezie <ucheemekaekemezie@gmail.com>
2024-11-28 11:31:45 +0100

    added missed commits

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
 
 @app.get("/")
 def read_root():
@@ -9,3 +15,9 @@ def read_root():
 @app.get("/posts")
 def get_posts():
     return {"data": "This is your post."}
+
+
+@app.post("/createposts")
+def create_posts(new_post: Post):
+    print(new_post)
+    return {"data": "new post"}
